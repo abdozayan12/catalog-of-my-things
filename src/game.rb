8f9f62e9fa70ddfecc1c4a66ace74f91359e6f0b
MARVIN <o.silverygold@gmail.com>
2023-09-12 00:20:55 +0300

    Add method to list all games

--- src/game.rb
+++ src/game.rb
@@ -2,11 +2,14 @@ require_relative 'items'
 
 class Game < Items
   attr_accessor :multiplayer, :last_played_at
+  attr_reader :games
 
+  @games = []
   def initialize(multiplayer, last_played_at)
     super()
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+    self.class.games << self
   end
 
   private
@@ -15,5 +18,11 @@ class Game < Items
     today = Date.today
     year_diff = (today.year - publish_date.year).abs
     super.can_be_archived && year_diff > 2
+  end
+
+  def list_all_games
+    @games.each_with_index do |game, index|
+      puts "#{index + 1}. Title: #{game.label}, Genre: #{game.genre}"
+    end
   end
 end
