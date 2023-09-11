f91612dde79a744e4c478ce796ee07404f8b1ce9
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-11 19:21:23 +0300

    Add setters and getters for Genre class

--- src/genre.rb
+++ src/genre.rb
@@ -1,9 +1,12 @@
 require_relative 'items'
 
 class Genre
-    def initialize(name)
-      @id = Random.rand(1..100)
-      @name = name
-      @items = []
-    end
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1..100)
+    @name = name
+    @items = []
+  end
 end
