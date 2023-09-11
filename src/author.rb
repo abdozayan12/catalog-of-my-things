76dd39f5437c53377947093c6d62bf019bede198
MARVIN <o.silverygold@gmail.com>
2023-09-12 00:58:50 +0300

    Add author class and list_authors method

---
+++ src/author.rb
@@ -0,0 +1,24 @@
+class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :authors
+
+  @authors = []
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+    self.class.authors << self
+  end
+
+  def add_item(item)
+    items << item
+  end
+
+  def list_authors
+    @authors.each_with_index do |author, index|
+      puts "#{index + 1}.#{author.first_name} #{author.last_name}"
+    end
+  end
+end
