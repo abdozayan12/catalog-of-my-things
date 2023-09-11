e1f7e08965c213819f3ccdf0da920b6c6b7f7aae
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-11 19:12:17 +0300

    Update music_album.rb

--- src/music_album.rb
+++ src/music_album.rb
@@ -1,6 +1,8 @@
 require_relative 'items'
 
 class MusicAlbum < Items
+  attr_accessor :on_spotify
+
   def initialize(on_spotify, publish_date)
     super(publish_date)
     @on_spotify = on_spotify
