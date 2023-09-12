a7585c831cf7680a268a5ee7cffd011ec8d8be37
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-12 16:09:22 +0300

    update

--- src/classes/music_album.rb
+++ src/classes/music_album.rb
@@ -1,19 +1,13 @@
 require_relative '../items'
 
 class MusicAlbum < Items
-  attr_accessor :on_spotify, :archived
+  attr_accessor :on_spotify, :archived, :publish_date
 
   def initialize(on_spotify, publish_date)
     super()
     @on_spotify = on_spotify
     @publish_date = publish_date
     @archived = false
-  end
-
-  attr_reader :publish_date
-
-  def publish_date=(new_date)
-    @publish_date = new_date
   end
 
   def can_be_archived?
--- src/items.rb
+++ src/items.rb
@@ -1,10 +1,32 @@
 class Items
-  @@next_id = 1
+  attr_accessor :genre, :author, :source, :label, :publish_date
 
-  attr_reader :id
+  def initialize(genre, author, source, label, publish_date)
+    @id = Random.rand(1..1_000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
 
-  def initialize
-    @id = @@next_id
-    @@next_id += 1
+  def add_genre(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    self.archived = true
+  end
+
+  private
+
+  def can_be_archived?
+    today = Date.today
+    year_diff = (today.year - publish_date.year).abs
+    year_diff > 10
   end
 end
