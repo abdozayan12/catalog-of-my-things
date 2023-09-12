024f078a9197b257b86f594311e9e38dc290f276
migo1 <ertmigot@gmail.com>
2023-09-12 05:25:31 +0300

    fix-linters

--- src/label.rb
+++ src/label.rb
@@ -1,11 +1,11 @@
 class Label
   attr_reader :id, :items
   attr_accessor :title, :color
-  
-  @@initial_id = 0
-  def initialize(title, color, items)
-    @@initial_id += 1
-    @id = @@initial_id
+
+  @initial_id = 0
+  def initialize(title, color, _items)
+    self.class.increment_initial_id
+    @id = self.class.access_initial_id
     @title = title
     @color = color
     @items = []
@@ -14,5 +14,13 @@ class Label
   def add_item(item)
     @items << item
     item.label = self
+  end
+
+  def self.increment_initial_id
+    @initial_id += 1
+  end
+
+  def self.access_initial_id
+    @initial_id
   end
 end
