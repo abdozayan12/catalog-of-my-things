1c0f00398cf5703f4693ed47a387927a933c3381
MARVIN <o.silverygold@gmail.com>
2023-09-12 21:30:42 +0300

    Add game and list games functionalities

--- src/game.rb
+++ src/game.rb
@@ -1,26 +1,74 @@
 require_relative 'items'
+require_relative 'author'
+require 'json'
 
 class Game < Items
   attr_accessor :multiplayer, :last_played_at
   attr_reader :games
 
   @games = []
-  def initialize(options = {})
-    super(options[:genre], options[:author], options[:source], options[:label], options[:publish_date])
-    @multiplayer = options[:multiplayer]
-    @last_played_at = options[:last_played_at]
+  def initialize(publish_date, multiplayer, last_played_at, _id = nil)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
 
-  def add_game(options = {})
-    game = Game.new(options)
-    self.class.games << game
-    'New Game added'
-  end
-
-  def list_all_games
-    self.class.games.each_with_index do |game, index|
-      puts "#{index + 1}. Title: #{game.label}, Genre: #{game.genre}"
+  def self.add_game()
+    puts 'Enter publish date'
+    pub_date = gets.chomp
+    puts 'Is the game multiplayer(1) or not multiplayer(2) [Enter number [1 or 2]]'
+    det = gets.chomp.to_i
+    if det == 1
+      multiplayer = true
+    elsif det == 2
+      multiplayer = false
     end
+
+    puts 'Enter the date the game was last played'
+    last_played = gets.chomp
+    new_game = Game.new(pub_date, multiplayer, last_played)
+    @games << new_game
+    Game.file_writer
+    puts 'New Game added'
+  end
+
+  def self.list_all_games
+    if File.exist?('games.json')
+      games_json = File.read('games.json')
+      games_data = JSON.parse(games_json)
+      @games.clear
+      games_data.each do |data|
+        game = Game.new(data['publish_date'], data['multiplayer'], data['last_played_at'])
+        @games << game
+      end
+    end
+
+    if @games.empty?
+      puts 'No games to display'
+    else
+      @games.each_with_index do |game, index|
+        puts "#{index + 1}. Published on: #{game.publish_date}, " \
+             "Multiplayer: #{game.multiplayer}, " \
+             "Last played: #{game.last_played_at}"
+      end
+    end
+  end
+
+  def self.file_writer
+    File.write('games.json', @games.map(&:to_hash).to_json)
+  end
+
+  def to_hash
+    {
+      id: @id,
+      publish_date: @publish_date,
+      multiplayer: @multiplayer,
+      last_played_at: @last_played_at
+    }
+  end
+
+  def to_json(*args)
+    to_hash.to_json(*args)
   end
 
   private
