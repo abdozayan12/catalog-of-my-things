6de4d0bca4adcbdddb7246e884c45624724aa8d3
migo1 <ertmigot@gmail.com>
2023-09-12 16:17:55 +0300

    update-items

--- src/items.rb
+++ src/items.rb
@@ -1,14 +1,25 @@
 class Items
-  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_accessor :publish_date, :archived
+  attr_reader :genre, :author, :label
 
-  def initialize(genre, author, source, label, publish_date)
+  def initialize(publish_date)
     @id = Random.rand(1..1_000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
     @publish_date = publish_date
-    @archived = false
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items << self unless genre.add_item.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.add_item.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items << self unless label.add_item.include?(self)
   end
 
   def move_to_archive
