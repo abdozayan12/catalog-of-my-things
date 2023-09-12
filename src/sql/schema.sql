282efd517875ca7cd6194f4e97434f6c928c27f4
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-12 10:35:00 +0300

    Add music_album and genre create table

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -9,3 +9,19 @@ CREATE TABLE item (
   archived        boolean
   PRIMARY KEY(id)
 );
+
+-- Create MusicAlbum Table
+CREATE TABLE music_album (
+  id              integer GENERATED ALWAYS AS IDENTITY,
+  publish_date    date,
+  on_spotify      boolean,
+  genre_id        int references genre(id),
+  PRIMARY KEY(id)
+);
+
+-- Create Genre Table
+CREATE TABLE genre(
+  id              integer GENERATED ALWAYS AS IDENTITY,
+  name            varchar(100),
+  PRIMARY KEY(id)
+);
